a2c5762b7bf698987e766f42d6cbb6211eb9c2bf
Natalia Kuntić <nataliakuntic@Natalias-MacBook-Pro.local>
2023-04-07 21:53:00 +0200

    Presents First Component - single Todo Item

---
+++ src/TodoItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function TodoItem(props) {
+  return (
+    <div className="checkbox">
+      <input
+        id={props.id}
+        type="checkbox"
+        defaultChecked={props.completed}
+        onChange={() => props.toggleTaskCompleted(props.id)}
+      />
+      <label className="todo-label" htmlFor={props.id}>
+        {props.name}
+      </label>
+    </div>
+  );
+}
