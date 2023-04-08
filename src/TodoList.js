9c17b8d0945edf03748e56495c39a5b2e8d62828
Natalia Kuntić <nataliakuntic@Natalias-MacBook-Pro.local>
2023-04-09 00:32:13 +0200

    REPLACES "PROPS" WITH "TODO" IN THE MAP
    props.id replaced by todo.id
    props.title replaced by todo.title
    props.isCompleted replaced by todo.isCompleted

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,11 +3,7 @@ import TodoItem from './TodoItem';
 
 export default function TodoList(props) {
   const taskList = props.todos.map(todo => (
-    <TodoItem
-      id={props.id}
-      title={props.title}
-      isCompleted={props.isCompleted}
-    />
+    <TodoItem id={todo.id} title={todo.title} isCompleted={todo.isCompleted} />
   ));
 
   return <div className="taskList">{taskList}</div>;
