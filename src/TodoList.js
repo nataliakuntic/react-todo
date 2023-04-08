c6f28ee208e0c3b134d794516a4994ef1c6236fc
Natalia Kuntić <nataliakuntic@Natalias-MacBook-Pro.local>
2023-04-09 00:24:19 +0200

    ADDS NEW VERSION OF COMPONENT TODO LIST
    Upon suggestions, this is my second version of TodoList with the instances.

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,13 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 export default function TodoList(props) {
-  const taskList = props.TodoItem.map(task => TodoItem);
+  const taskList = props.todos.map(todo => (
+    <TodoItem
+      id={props.id}
+      title={props.title}
+      isCompleted={props.isCompleted}
+    />
+  ));
 
   return <div className="taskList">{taskList}</div>;
 }
