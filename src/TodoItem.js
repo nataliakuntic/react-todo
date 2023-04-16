4f975dc4442f102b10f6b8cf29b19f1c4c0cee7f
Natalia Kuntić <nataliakuntic@Natalias-MacBook-Pro.local>
2023-04-16 13:14:08 +0200

    ADDS ABILITY TO TOGGLE THE CHECKBOX IN TODOITEM
    Created function todoToggleCompleted and corrected "checked" into "isChecked" in the input element in TodoItem component in issue #10.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function TodoItem(props) {
+export default function TodoItem(todo) {
+  const [newTodo, setNewTodo] = useState([]);
+  function toggleTodoCompleted(id) {
+    const updatedTodos = newTodo.map(todo => {
+      if (id === todo.id) {
+        return { ...todo, isCompleted: !todo.isCompleted };
+      }
+      return todo;
+    });
+    setNewTodo(updatedTodos);
+  }
+
   return (
     <div className="checkbox">
       <input
-        id={props.id}
+        id={todo.id}
         type="checkbox"
-        checked={props.isCompleted}
-        onChange={() => props.toggleTaskCompleted(props.id)}
+        isChecked={todo.isCompleted} //it's "isChecked" and not "checked" because it's a React component
+        onChange={() => toggleTodoCompleted(todo.id)}
       />
-      <label className="todo-label" htmlFor={props.id}>
-        {props.title}
+      <label className="todo-label" htmlFor={todo.id}>
+        {todo.title}
       </label>
     </div>
   );
