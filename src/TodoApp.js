d5236c2f019e41a1e2d4ab116a6cc7becf0c2413
Natalia Kuntić <nataliakuntic@Natalias-MacBook-Pro.local>
2023-04-23 20:40:43 +0200

    ADDS FILTERBUTTON.JS AND IMPLEMENTS THIS FEATURE IN TODOAPP AND TODOLIST
    I made a file called FilterButton. It contains buttons that on click filter out if the todo is pending or completed (or shows all of them). It's passed as a prop from the parent. TodoList has a filter and TodoApp has FILTER_MAP.

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -2,9 +2,19 @@ import { nanoid } from 'nanoid';
 import React, { useState } from 'react';
 import TodoList from './TodoList';
 import AddTodo from './AddTodo';
+import FilterButton from './FilterButton';
+
+const FILTER_MAP = {
+  all: () => true,
+  pending: todo => !todo.isCompleted,
+  completed: todo => todo.isCompleted,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 export default function TodoApp(props) {
   const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState('all');
 
   const addTodo = title => {
     const newTodo = { id: `todo-${nanoid()}`, title, isCompleted: false };
@@ -21,10 +31,24 @@ export default function TodoApp(props) {
     setTodos(updatedTodos);
   }
 
+  const filterList = FILTER_NAMES.map(title => (
+    <FilterButton
+      key={title}
+      name={title}
+      isPressed={title === filter}
+      setFilter={setFilter}
+    />
+  ));
+
   return (
     <div className="todo-list">
       <AddTodo addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodoCompleted={toggleTodoCompleted} />
+      {filterList}
+      <TodoList
+        todos={todos}
+        toggleTodoCompleted={toggleTodoCompleted}
+        filter={FILTER_MAP[filter]}
+      />
     </div>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,9 +2,10 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 export default function TodoList(props) {
-  const taskList = props.todos.map(todo => (
-    <TodoItem todo={todo} toggleTodoCompleted={props.toggleTodoCompleted} />
-  ));
-
+  const taskList = props.todos
+    .filter(props.filter)
+    .map(todo => (
+      <TodoItem todo={todo} toggleTodoCompleted={props.toggleTodoCompleted} />
+    ));
   return <div className="task-list">{taskList}</div>;
 }
